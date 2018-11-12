03e1aa4e75620c4ff08cd352f9726c3699d42ca7
rayanwipes <rayan@heywipe.com>
2018-11-12 12:12:27 +0000

    d

--- build/env/defaults.js
+++ build/env/defaults.js
@@ -5,11 +5,11 @@ var config = {
     port: process.env.PORT || 3000,
     env: 'development',
     database: {
-        client: 'mongodb'
+        client: 'mongodb://heroku_sz1xtlxd:m1go0e6822ttstpvbec0rvt4ob@ds159673-a0.mlab.com:59673,ds159673-a1.mlab.com:59673/heroku_sz1xtlxd?replicaSet=rs-ds159673'
     }
 };
 // Set the current environment or default to 'development'
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 config.env = process.env.NODE_ENV;
 exports.default = config;
 //# sourceMappingURL=defaults.js.map
--- src/env/defaults.ts
+++ src/env/defaults.ts
@@ -4,12 +4,12 @@ const config: any = {
     port: process.env.PORT || 3000,
     env: 'development',
     database: {
-        client: 'mongodb'
+        client: 'mongodb://heroku_sz1xtlxd:m1go0e6822ttstpvbec0rvt4ob@ds159673-a0.mlab.com:59673,ds159673-a1.mlab.com:59673/heroku_sz1xtlxd?replicaSet=rs-ds159673'
     }
 };
 
 // Set the current environment or default to 'development'
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 config.env = process.env.NODE_ENV;
 
 export default config;
